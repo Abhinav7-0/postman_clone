5b8d02fbffe75fcba96b7c20801e88fa9576833b
Abhinav7-0 <btech10290.19@bitmesra.ac.in>
2023-12-20 19:43:28 +0530

    Changes in Header

--- src/Components/URL_NavBar/Header.jsx
+++ src/Components/URL_NavBar/Header.jsx
@@ -2,8 +2,7 @@ import {makeStyles, Box} from '@material-ui/core';
 
 const useStyles = makeStyles({
     logo: {
-        width:150,
-        padding: 5,
+        width:160,
         marginTop: 4,
     },
     Component: {
@@ -13,10 +12,13 @@ const useStyles = makeStyles({
 
 const Header = () => {
     const classes = useStyles();
+    const logo = "https://www.dcvelocity.com/ext/resources/user-submissions/articles/bea7ca44-08a4-44ff-a5af-622605a12fb5.png?height=635&t=1602116058&width=1200000000000"
+
+   // "https://www.dcvelocity.com/ext/resources/user-submissions/articles/bea7ca44-08a4-44ff-a5af-622605a12fb5.png?height=635&t=1602116058&width=1200"
 
     return(
         <Box className={classes.Component}>
-            <img src="https://www.dcvelocity.com/ext/resources/user-submissions/articles/bea7ca44-08a4-44ff-a5af-622605a12fb5.png?height=635&t=1602116058&width=1200" alt="logo" className={classes.logo}/>
+            <img src={logo} alt="logo" className={classes.logo}/>
         </Box>   
     )
 }
